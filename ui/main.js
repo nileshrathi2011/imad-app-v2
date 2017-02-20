031bd259f4525cf28b739c7e90b080ff18d677cb
nileshrathi2011 <nileshrathi2011@gmail.com>
2017-02-20 19:49:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,8 +6,13 @@ element.innerHTML='new Value';
 // move the image
 
 var img=document.getElementById('madi');
-
+var marginLeft=0;
+function moveRight()
+{
+ marginLeft=magginLeft+10;
+ img.style.marginLeft= magginLeft+ 'px';
+}
 img.onclick=function()
 {
-  img.style.marginLeft='100px';  
+  var interval=setInterval(moveRight,100); 
 };
