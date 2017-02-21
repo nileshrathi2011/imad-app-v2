53c1bba5e1c3e9c8b7596c3fef2f9de09d21680f
nileshrathi2011 <nileshrathi2011@gmail.com>
2017-02-21 20:07:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ button.onclick=function()
 };
 var nameinput = document.getElementById('name');
 var nameValue = nameInput.value();
-var submit = document.getElementById('submit-btn');
+var submit = document.getElementById('submit_btn');
 submit.onclick=function()
 {
    var names=['name1','name2','name3','name4'];
