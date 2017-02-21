179da4d668d2a040af1a0e5016ef1d73541bcd3f
nileshrathi2011 <nileshrathi2011@gmail.com>
2017-02-21 19:10:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,23 @@
 var button=document.getElementById("counter");
-var counter=0;
+
 button.onclick=function()
 {
-    var span = document.getElementById("count");
-    counter=counter+1;
-    span.innerHTML=counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+      if(request.ReadyState===XMLHttpRequest.DONE)
+      {
+          if(request.status===200)
+          {
+              var counter= request.responseText;
+               var span = document.getElementById("count");
+               span.innerHTML=counter.toString();
+              
+          }
+      }
+    };
+    
+   
+    
+    
 };
