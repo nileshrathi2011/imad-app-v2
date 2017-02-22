b2090536db336008c058658e99c4503325f31fd4
nileshrathi2011 <nileshrathi2011@gmail.com>
2017-02-22 12:26:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ submit.onclick=function()
    var list='';
    for(var i=0;i<names.length;i++)
    {
-       list+= '<li>' + names[i] + 'fdsf</li>';
+       list+= '<li>' + names[i] + '</li>';
    }
    var ul= document.getElementById("name_list");
    
