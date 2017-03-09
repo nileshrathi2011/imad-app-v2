4f012937e78073e88d5f9fadca1671cb9410eb6b
nilesh rathi <nileshrathi2011@gmail.com>
2017-03-09 17:33:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config={
   database:'nileshrathi2011',
   host:'db.imad.hasura-app.io',
   port:'5432',
-  password: process.env.DB_PASSWORD
+  password: 'db-nileshrathi2011-83661'
 };
 var app = express();
 app.use(morgan('combined'));
@@ -96,7 +96,7 @@ app.get('/test-db',function(req,res){
        if(err)
             res.status(500).send(err.toString());
         else
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
     });      
 });
 
