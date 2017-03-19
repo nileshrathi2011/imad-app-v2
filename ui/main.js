6fb7614e565482a99558e0d620927394f535b82b
nilesh rathi <nileshrathi2011@gmail.com>
2017-03-19 15:01:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,7 +60,7 @@ submit.onclick=function()
      console.log(password);
     request.open('POST','http://nileshrathi2011.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stingify({username: username , password: password}));
+    request.send(JSON.stringify({username: username , password: password}));
   
    
 };
